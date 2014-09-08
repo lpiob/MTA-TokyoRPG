1d5acfc899431f6bfe76d9a35fb6c2b97a108d62
ThePiotrek2003 <piotrfilmy@wp.pl>
2014-09-08 10:59:19 +0200

    Update live_die_s.lua

--- gamemode/RL_BASE/live_die_s.lua
+++ gamemode/RL_BASE/live_die_s.lua
@@ -1,9 +1,9 @@
-addEventHandler("onPlayerWasted",getRootElement(),function (player)
-player = source
+addEventHandler("onPlayerWasted",getRootElement(),function ()
+
 setTimer( function () spawnPlayer(player,-2100.265625,1881.0595703125,82.2734375) 
 setElementDimension(player,0)
 setElementInterior(player,0)
-     outputChatBox("#63DBFF*Zginąłęś i zostałeś przewieziony do szpitala płacisz #B9F46C100$#63DBFF za podatek medyczny.",player,0,0,0,true)
+     outputChatBox("#63DBFF*Zginąłęś/aś i zostałeś/aś przewieziony/a do szpitala płacisz #B9F46C100$#63DBFF za opiekę medyczną.",player,0,0,0,true)
     takePlayerMoney(player,100)
     end,2000,1)
 end
