e78ac26dc9a409139028f70430774ff0b0293e1f
ThePiotrek2003 <piotrfilmy@wp.pl>
2014-09-08 13:53:50 +0200

    Create autom_s.lua
    Automatyczne wiadomości.

---
+++ gamemode/RL_MISC/autom_s.lua
@@ -0,0 +1,31 @@
+
+
+local messages = {
+
+{"[INFO] Witaj na gamemodzie TokyoRPG, życzymy Ci miłej gry."},
+{"[INFO] Jeżeli nie przepadasz za tradycyjnym zarabianiem, możesz porobić sztuczki BMX - za to także jest profit."},
+{"[INFO] Wiadomości te możesz zmienić w RL_MISC/autom_s.lua"},
+
+}
+
+
+
+
+
+setTimer(
+function ( )
+local msg = unpack(messages[math.random(#messages)])
+outputChatBox(msg)
+end, 500000, 0)
+
+-- 500000 to czas po ktorym zostanie wyswietlona nastepna wiadomosc w ms.
+-- mozna przeliczyc sekundy na ms na przyklad tutaj: http://www.unitconversion.org/time/seconds-to-milliseconds-conversion.html
+-- w tym przypadku wiadomosc bedzie sie wyswietlala co 8 minut, chociaz jak jest malo graczy to polecam zwiekszyc
+-- bo bedzie pewnie caly czat zaspamiony.
+
+
+
+
+
+
+
