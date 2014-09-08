5d3252288d22a48e0bef7016c918d2cd2d435a30
ThePiotrek2003 <piotrfilmy@wp.pl>
2014-09-08 14:48:25 +0200

    Update 2wheeler_s.lua

--- gamemode/RL_STUNT/2wheeler_s.lua
+++ gamemode/RL_STUNT/2wheeler_s.lua
@@ -2,7 +2,7 @@
 
 addEvent("2WheelerCash", true)
 addEventHandler("2WheelerCash", root,
-function (time, distance )
+function ( distance, time )
   
   local wynagrodzenie = math.floor(distance)*math.floor(time) -- robione na oko, pewnie bedzie trzeba potem zmienic
 givePlayerMoney(source, wynagrodzenie)
