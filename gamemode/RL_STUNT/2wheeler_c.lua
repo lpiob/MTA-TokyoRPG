21773c4da6f8216632e7dd263c46b8b5255a132f
ThePiotrek2003 <piotrfilmy@wp.pl>
2014-09-08 14:47:09 +0200

    Update 2wheeler_c.lua

--- gamemode/RL_STUNT/2wheeler_c.lua
+++ gamemode/RL_STUNT/2wheeler_c.lua
@@ -3,8 +3,7 @@
 addEventHandler("onClientPlayerStuntFinish",
 function (type, time, distance)
 if type = "2wheeler" then
-local wynagrodzenie = math.floor(distance)*math.floor(time) -- robione na oko, pewnie bedzie trzeba potem zmienic
 outputChatBox("Otrzymujesz "..wynagrodzenie.."$ za jazdę na dwóch kołach przez "..time.." na dystansie "..distance, source)
-triggerServerEvent("2WheelerCash", source)
+triggerServerEvent("2WheelerCash", source, distance, time)
 end
 end)
